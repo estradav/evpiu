adca0f47529ee4310c69c25c0876259e8a1e0143
Santiago Arboleda Londoño <msarboleda@estradavelasquez.com>
2019-08-05 11:45:01 -0500

    Get groups of many permissions

--- app/Permission.php
+++ app/Permission.php
@@ -14,4 +14,21 @@ class Permission extends \Spatie\Permission\Models\Permission
     {
         return $this->belongsTo(PermissionGroup::class, 'group_id');
     }
+
+    /**
+     * Obtiene el grupo al que pertenece cada permiso del parámetro.
+     *
+     * @param  array $permissions
+     * @return \Spatie\Permission\Models\Permission
+     */
+    public static function getGroups(array $permissions) {
+        return array_map(function ($permission){
+            return static::where('id', $permission)
+                        ->with('group')
+                        ->get()
+                        ->first()['group'];
+        }, $permissions);
+    }
+
+
 }
