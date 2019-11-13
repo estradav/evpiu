1769e465e606e80e15954e4a03bbeb6ba9a46421
dcorrea-estrav <54317215+dcorrea-estrav@users.noreply.github.com>
2019-11-13 15:04:57 -0500

    Mejoras visuales minimas

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,17 @@
+<title>Sitio en mantenimiento - EVPIU</title>
+<style>
+    body { text-align: center; padding: 150px; }
+    h1 { font-size: 50px; }
+    body { font: 20px Helvetica, sans-serif; color: #333; }
+    article { display: block; text-align: left; width: 650px; margin: 0 auto; }
+    a { color: #f49b00; text-decoration: none; }
+    a:hover { color: #333; text-decoration: none; }
+</style>
+<img src="/img/Logo_v2.png" alt="Descripción de la imagen" style="text-align: center; ">
+<article>
+    <h1>¡Estaremos de vuelta Pronto...!</h1>
+    <div>
+        <p align="justify">Disculpe las molestias, pero estamos realizando algunas tareas de mantenimiento en este momento. Si lo necesita, puede <a href="mailto:auxsistemas@estradavelasquez.com">contactarnos</a>, de lo contrario, volveremos a estar en linea en breve!</p>
+        <p>&mdash; Equipo de sistemas.</p>
+    </div>
+</article>
