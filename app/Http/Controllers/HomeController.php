0fd13e740101f654193515b8259dc76b415991fa
dcorrea-estrav <54317215+dcorrea-estrav@users.noreply.github.com>
2020-02-24 08:01:37 -0500

    Update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,4 +25,9 @@ class HomeController extends Controller
     {
         return view('home');
     }
+
+    public function AccesosRemotos()
+    {
+        return view('accesos_remotos');
+    }
 }
