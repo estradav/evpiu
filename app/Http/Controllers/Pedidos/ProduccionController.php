df4755431436787acd2053b181bb05c89232fc84
Daniel Hincapié <54317215+Ryunosukee@users.noreply.github.com>
2021-03-31 12:09:16 -0500

    Update ProduccionController.php

--- app/Http/Controllers/Pedidos/ProduccionController.php
+++ app/Http/Controllers/Pedidos/ProduccionController.php
@@ -567,7 +567,7 @@ class ProduccionController extends Controller
                             ]);
                         $idx++;
                     }
-                    DB::connection('MAX')->select('UPDATE Requirement_Detail SET CURDUE_11 = CONVERT(date,CURDUE_11,23) WHERE ORDNUM_11 = '$max_ordnum_27);
+                    DB::connection('MAX')->select(`UPDATE Requirement_Detail SET CURDUE_11 = CONVERT(date ,CURDUE_11, 23) WHERE ORDNUM_11 = $max_ordnum_27`);
                     $pedido->Ped_MAX = $max_ordnum_27;
                     $pedido->save();
                 }
